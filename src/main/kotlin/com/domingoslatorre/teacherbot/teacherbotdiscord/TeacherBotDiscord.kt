8a76c41af045bad1748a689e719e4f92f59932e7
Domingos Latorre <domingos.latorre@gmail.com>
2021-03-23 00:17:11 -0300

    Refactor main method

--- src/main/kotlin/com/domingoslatorre/teacherbot/teacherbotdiscord/TeacherBotDiscord.kt
+++ src/main/kotlin/com/domingoslatorre/teacherbot/teacherbotdiscord/TeacherBotDiscord.kt
@@ -1,10 +1,9 @@
 package com.domingoslatorre.teacherbot.teacherbotdiscord
 
+import com.domingoslatorre.teacherbot.teacherbotdiscord.forms.Form
+import com.domingoslatorre.teacherbot.teacherbotdiscord.listeners.MemberJoinListener
+import com.domingoslatorre.teacherbot.teacherbotdiscord.listeners.MessageReceiveListener
 import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.entities.ChannelType
-import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent
-import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.requests.GatewayIntent
 
 fun getBotToken() = when (val token = System.getenv("DISCORD_BOT_TOKEN")) {
@@ -12,33 +11,15 @@ fun getBotToken() = when (val token = System.getenv("DISCORD_BOT_TOKEN")) {
     else -> token
 }
 
-fun MessageReceivedEvent.isFromMember() = this.isFromType(ChannelType.TEXT) && ! this.author.isBot
-fun MessageReceivedEvent.reply(message: String) = this.channel.sendMessage(message).queue()
-
-object MessageReceiveListener : ListenerAdapter() {
-    override fun onMessageReceived(event: MessageReceivedEvent) {
-        if(event.isFromMember() && event.message.contentRaw == ".hi") {
-            event.reply("Hello")
-        }
-    }
-}
-
-object MemberJoinListener : ListenerAdapter() {
-    override fun onGuildMemberJoin(event: GuildMemberJoinEvent) {
-        event.user.openPrivateChannel().queue() {
-            it.sendMessage("Welcome ${event.user.name}").queue()
-        }
-    }
-}
-
 fun main() {
+    val inMemoryForms = mutableMapOf<String, Form>()
+
     val jda = JDABuilder.createDefault(getBotToken())
         .enableIntents(GatewayIntent.GUILD_MEMBERS)
         .build()
 
     jda.addEventListener(
-        MessageReceiveListener,
+        MessageReceiveListener(inMemoryForms),
         MemberJoinListener,
     )
-
 }
