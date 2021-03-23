354d56e00be256b006a47b64b931a2965e2bd548
Domingos Latorre <domingos.latorre@gmail.com>
2021-03-23 09:43:24 -0300

    Configure kotest in build.gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,4 +14,18 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("net.dv8tion:JDA:4.2.0_241")
     implementation("org.slf4j:slf4j-simple:1.7.30")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.4.3")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.4.3")
+    testImplementation("io.kotest:kotest-property-jvm:4.4.3")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict", "-Xallow-result-return-type")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
